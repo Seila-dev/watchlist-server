635a8c129dbcef16affa3d9cc339b2047df07717
Erick Rodrigues <erickrodrigues3975@gmail.com>
2025-09-04 12:10:32 -0300

    fix: add config to errors on jsconfig and fix errors

--- src/routes/ContentRoutes.js
+++ src/routes/ContentRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import ContentController from '../controllers/ContentController.js';
-import auth from '../middlewares/Auth.js';
+// import auth from '../middlewares/Auth.js';
 
 const router = Router();
 
-router.get('/contents', auth, (req, res) => ContentController.getContents(req, res));
+router.get(
+    '/contents', 
+    // auth, 
+    (req, res) => ContentController.getContents(req, res)
+);
 
 export default router;
